c828b32dc96c6fa885f177dbbd54911c9d73aa16
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-15 20:47:21 -0300

    refactor: remove unused About and Blog pages

--- src/pages/blog/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlogPage() {
-  return (
-    <div>
-      <h2>blog</h2>
-    </div>
-  );
-}
--- src/pages/blog/posts/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function PostsPage() {
-    return (
-      <div>
-        <h2>posts</h2>
-      </div>
-    );
-  }
-  
