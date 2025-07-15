c74853792cc39d9cb80c5bdda304ed679921f977
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-15 18:09:56 -0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+} satisfies Config;
