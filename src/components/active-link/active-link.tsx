ef15e2bff3c6a90f1554e2201b8c2c98695e864e
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-20 18:41:03 -0300

    feat: add ActiveLink component for active navigation highlighting

---
+++ src/components/active-link/active-link.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+
+type ActiveLinkProps = {
+  children: React.ReactNode
+} & LinkProps
+
+export const ActiveLink = ({ href, children, ...rest }: ActiveLinkProps) => {
+  const router = useRouter()
+  const isCurrentPath = router.asPath === href || router.asPath === rest.as || router.asPath.startsWith(String(rest.as))
+
+  return <Link href={href} className={cn('text-sm font-medium transition-colors hover:text-primary hover:text-blue-500', isCurrentPath ? 'text-blue-500' : 'text-muted-foreground')}>{children}</Link>
+}
