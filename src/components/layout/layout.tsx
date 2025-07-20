b4c5fd611b9637c27457b0128944a7d5fa096678
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-20 19:37:11 -0300

    feat: create layout index file and export layout components

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,28 +1,30 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "../header"
+import { Footer } from "../footer";
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
-const geistSans = Geist({
+export const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
   });
   
-  const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-  });
+export const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
   
 export const Layout = ({ children }: LayoutProps) => {
 
   return (
-    <div className={`${geistSans.className} ${geistMono.className} relative flex min-h-screen flex-col dark`}>
+    <div className={`relative flex min-h-screen flex-col dark`}>
       <Header />
-      <main className="flex-1 flex flex-col mb-12">
+      <main className="flex flex-col mb-12">
         {children}
       </main>
+      <Footer />
     </div>
   )
 }
