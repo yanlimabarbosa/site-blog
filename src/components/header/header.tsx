f0959da82b5bc94389cf307133bfc4c2dcb22ac9
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-20 19:36:58 -0300

    refactor: enhance Header component with typography and Logo integration

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { ThemeToggle } from "../ui/theme-toggle";
 import { ActiveLink } from "../active-link";
+import { geistMono, geistSans } from "../layout";
+import { Logo } from "../logo";
 
 export function Header() {
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx- max-w-7xl px-4 sm:px-6 lg: px-8">
+    <header className={`${geistSans.className} ${geistMono.className} fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60`}>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg: px-8">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/" >Logo</Link>
-
+          <Logo />
+    
           <nav className="flex items-center gap-6">
             <ThemeToggle />
             <ActiveLink href="/">Início </ActiveLink>
