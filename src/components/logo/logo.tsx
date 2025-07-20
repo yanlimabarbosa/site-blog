a868fc744bb9225715e80200b86f473c7801decc
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-20 19:36:43 -0300

    feat: add Logo component with image and link functionality

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Logo = () => {
+  return (
+    <Link href="/" title="Página inicial">
+      <Image src="/logo.svg" alt="Logo site" width={116} height={32}  className="hover:opacity-80 transition-opacity"/>
+    </Link>
+  );
+};
