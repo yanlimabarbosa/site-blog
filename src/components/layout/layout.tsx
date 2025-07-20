50faab68d31d1b76b921f5b43a59af1236114d39
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-20 20:36:12 -0300

    feat: hero section

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -21,9 +21,9 @@ const inter = Inter({subsets:["latin"]})
 export const Layout = ({ children }: LayoutProps) => {
 
   return (
-    <div className={`${inter.className} relative flex min-h-screen flex-col dark`}>
+    <div className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}>
       <Header />
-      <main className="flex flex-col mb-12">
+      <main className="flex-1 flex-col mb-12">
         {children}
       </main>
       <Footer />
